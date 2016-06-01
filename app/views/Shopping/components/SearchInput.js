4d8cab38c470f08f39a7d097d610b9dbeeac85b9
Riley Bracken <rileybracken@gmail.com>
2016-06-01 09:05:39 -0600

    Fixes the placeholder on search input

--- app/views/Shopping/components/SearchInput.js
+++ app/views/Shopping/components/SearchInput.js
@@ -13,25 +13,36 @@ const SearchInput = ({
   navigator,
   toggleSearchOverlay,
   placeholder
-}) => (
-  <View style={ style } >
-    <View style={ styles.inputContainer }>
-      <TextInput
-        style={ styles.input }
-        defaultValue={ placeholder }
-        placeholderTextColor='#fff'
-        selectionColor='#fff'
-        autoFocus={ true }
-        autoCorrect={ false }
-        returnKeyType='search'
-        enablesReturnKeyAutomatically={ true }
-        clearButtonMode='always'
-        onSubmitEditing={ requestProducts }
-      />
-      <Image source={ require('image!search') } />
+}) => {
+  let textInputProps = {
+    style: styles.input,
+    placeholder: 'Search',
+    placeholderTextColor: '#fff',
+    selectionColor: '#fff',
+    autoFocus: true,
+    autoCorrect: false,
+    returnKeyType: 'search',
+    enablesReturnKeyAutomatically: true,
+    clearButtonMode: 'always',
+    onSubmitEditing: requestProducts,
+  }
+
+  if (placeholder !== 'Search') {
+    textInputProps = {
+      ...textInputProps,
+      defaultValue: placeholder,
+    }
+  }
+
+  return (
+    <View style={ style } >
+      <View style={ styles.inputContainer }>
+        <TextInput { ...textInputProps } />
+        <Image source={ require('image!search') } />
+      </View>
     </View>
-  </View>
-)
+  )
+}
 
 const styles = StyleSheet.create({
   input: {
