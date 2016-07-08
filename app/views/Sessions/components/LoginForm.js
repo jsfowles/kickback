a5a7af6206c526a46887bfe72f0c7b9fb2f9540c
Riley Bracken <rileybracken@gmail.com>
2016-07-08 13:44:50 -0600

    Adds LoginForm component

---
+++ app/views/Sessions/components/LoginForm.js
@@ -0,0 +1,11 @@
+'use strict';
+import React from 'react';
+import { View, Text, } from 'react-native';
+
+const LoginForm = () => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>LoginForm</Text>
+  </View>
+);
+
+export default LoginForm;
