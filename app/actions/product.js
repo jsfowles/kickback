4ab5abad9d4caa818280669e348e2f9f2273a2e3
Riley Bracken <rileybracken@gmail.com>
2016-08-17 08:24:47 -0600

    You cannot recommend a product without being logged in.

--- app/actions/product.js
+++ app/actions/product.js
@@ -1,27 +1,36 @@
-'use strict'
+'use strict';
 
-import { createLink } from '../utils/api'
-import { ActionSheetIOS } from 'react-native'
+import { createLink } from '../utils/api';
+import { toggleSessionModal } from './sessions';
+import { ActionSheetIOS } from 'react-native';
 
-export const toggleCreatingRecommendation = (bool) => ({ type: 'TOGGLE_CREATING_RECOMMENDATION', bool })
+export const toggleCreatingRecommendation = (bool) => ({ type: 'TOGGLE_CREATING_RECOMMENDATION', bool });
 
 export const recommendProduct = (product) => {
-  return (dispatch) => {
-    dispatch(toggleCreatingRecommendation(true))
+  return (dispatch, getState) => {
+    const { user } = getState();
 
-    createLink(product).then((res) => {
-      dispatch(toggleCreatingRecommendation(false))
+    if (user.currentUser) {
+      // If user is currently logged in
+      dispatch(toggleCreatingRecommendation(true));
 
-      ActionSheetIOS.showShareActionSheetWithOptions(
-        { url: `http://www.${res.url}` },
-        (e) => console.error(e),
-        (success,method) => {
-          // TODO (Riley) : Show success
-        }
-      )
-    // TODO (Riley) : Show some type of error
-    }).catch((res) => {
-      dispatch(toggleCreatingRecommendation(false))
-    })
+      createLink(product, user.currentUser.id).then((res) => {
+        dispatch(toggleCreatingRecommendation(false));
+
+        ActionSheetIOS.showShareActionSheetWithOptions(
+          { url: `http://www.${res.url}` },
+          (e) => console.error(e),
+          (success,method) => {
+            // TODO (Riley) : Show success
+          }
+        )
+      // TODO (Riley) : Show some type of error
+      }).catch((res) => {
+        dispatch(toggleCreatingRecommendation(false));
+      });
+    } else {
+      // User is not logged in
+      dispatch(toggleSessionModal(true));
+    }
   }
-}
+};
