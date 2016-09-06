fb15ba766d780caad89cd3dada2fae9279ac6cef
Riley Bracken <rileybracken@gmail.com>
2016-09-06 12:08:00 -0600

    Pull session stuff out so it more describes a session.

--- app/reducers/__tests__/session-test.js
+++ app/reducers/__tests__/session-test.js
@@ -6,8 +6,41 @@ import { session } from '../session';
 
 jest.autoMockOff();
 
+const sessionObj = {
+  'uid': 'hello@underbelly.is',
+  'client': 'esEn6p0ftGs7v9_6pvxViw',
+  'expiry': 1474392944,
+  'token-type': 'Bearer',
+  'access-token': '88pu9HS1PVutlU59mH_kpw',
+};
+
 describe('session reducer', () => {
   it('is empty by default', () => {
     expect(session(undefined, {})).toEqual({});
+  });
+
+  it('sets session when logging in', () => {
+    expect(session(undefined, { type: 'CREATE_SESSION', session: sessionObj }))
+    .toEqual(sessionObj);
+  });
+
+  it('removes the session when logging out', () => {
+    expect(session(sessionObj, { type: 'DESTROY_SESSION' }))
+    .toEqual({});
+  });
+
+  describe('updates session when needed (typically when a post request comes back)', () => {
+    const newSessionObj = {
+      'uid': 'hello@underbelly.is',
+      'client': 'qQfDFKRfyQCDwR-hQY9vxQ',
+      'expiry': 1474394035,
+      'token-type': 'Bearer',
+      'access-token': 'iZC8bY3EbxpCMo5OjLNWFQ',
+    };
+
+    it('updates correctly', () => {
+      expect(session(sessionObj, { type: 'UPDATE_SESSION', session:  newSessionObj }))
+      .toEqual(newSessionObj);
+    });
   });
 });
--- app/reducers/session.js
+++ app/reducers/session.js
@@ -1,45 +1,11 @@
-const initialState = {
-  modalVisible: false,
-  currentSession: false,
-  session: null,
-  tabs: {
-    SIGN_UP: 'SIGN_UP',
-    LOG_IN: 'LOG_IN',
-  },
-  currentTab: 'SIGN_UP',
-  username: null,
-  showError: false,
-};
-
-export const session = (state = initialState, action) => {
+export const session = (state = {}, action) => {
   switch (action.type) {
-  case 'TOGGLE_SESSION_MODAL':
-    return {
-      ...state,
-      modalVisible: action.bool,
-    };
-  case 'SET_SESSION':
-    return {
-      ...state,
-      session: action.session,
-      currentSession: action.bool,
-      modalVisible: false,
-    };
-  case 'CHANGE_FORM':
-    return {
-      ...state,
-      currentTab: action.tab,
-    };
-  case 'UPDATE_USERNAME':
-    return {
-      ...state,
-      username: action.username,
-    };
-  case 'TOGGLE_ERROR':
-    return {
-      ...state,
-      showError: action.bool,
-    };
+  case 'CREATE_SESSION':
+    return action.session;
+  case 'UPDATE_SESSION':
+    return action.session;
+  case 'DESTROY_SESSION':
+    return {};
   default:
     return state;
   }
