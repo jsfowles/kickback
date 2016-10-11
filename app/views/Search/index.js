bb9491161424ea0c3eecc5b9b916bea6195dbcee
jsfowles <js.fowles@gmail.com>
2016-10-11 14:46:29 -0600

    added search return to SEARCH component

--- app/views/Search/index.js
+++ app/views/Search/index.js
@@ -1,45 +1,57 @@
 'use strict';
 
 import React from 'react';
-import { View, Text, TouchableHighlight } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 
 import Products from '../Products';
 import Container from '../shared/Container';
-import {
-  setHasScrolled,
-  scrollToTop,
-} from '../../actions';
+
 
 class Search extends React.Component {
+  static propTypes = {
+    search: React.PropTypes.shape({
+      errorMessage: React.PropTypes.string,
+      isFetching: React.PropTypes.bool,
+      searchOverlay: React.PropTypes.bool,
+      products: React.PropTypes.array,
+    }).isRequired,
+  };
+
   render() {
+    let { search } = this.props;
+
     return (
-      <View style={{ flex: 1, paddingTop: 65 }}>
-        <Text>Search</Text>
-        <TouchableHighlight
-          style={{ flex: 1 }}
-          underlayColor='pink'
-          onPress={ () => this.props.handleNavigate({ type: 'pop' }) }
-        >
-          <Text>Button</Text>
-        </TouchableHighlight>
-      </View>
+      <Container style={ styles.container }>
+        { search.isFetching ? (
+          <Text>Searching...</Text>
+        ) : (
+          <Products
+            products={ search.products }
+            cardSize={ 'large' }
+            title={ 'PRODUCTS FOUND' }
+          />
+        )}
+      </Container>
+
     );
   }
 }
 
-const mapStateToProps = (state) => ({
-  searchResults: state.search.searchResults,
-  searchOverlay: state.search.searchOverlay,
-  route: state.navigation.route,
-  searching: state.search.searching,
-  fetchingProducts: state.search.fetchingProducts,
-  hasScrolled: state.search.hasScrolled,
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 65,
+  },
 });
 
-const mapActionsToProps = (dispatch) => ({
-  setHasScrolled: () => dispatch(setHasScrolled('search')),
-  scrollToTop: () => dispatch(scrollToTop()),
+const mapStateToProps = (state) => ({
+  search: state.search,
+
+});
+
+const mapActionsToProps = (_) => ({
+
 });
 
 export default connect(mapStateToProps, mapActionsToProps)(Search);
