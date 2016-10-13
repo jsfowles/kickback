a6dd2fdf4b03f010760d5cba0673f47f91fcf11e
jsfowles <js.fowles@gmail.com>
2016-10-13 11:51:03 -0600

    Destroy Session now works and returns to feed tab

--- app/actions/tabs.js
+++ app/actions/tabs.js
@@ -2,13 +2,6 @@
 
 import { push } from './navigation';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> abb85ac... Destroy User now works and returns to feed tab
-=======
->>>>>>> 6c326f6b35df0be2faf8d272cb6d5f0bd0ec44ad
 export const changeTab = index => ({ type: 'CHANGE_TAB', index });
 
 export const onTabClick = index => (dispatch, getState) => {
