bbb67618a8d1f6989ed7bfce4e51565caaea4eb5
Riley Bracken <rileybracken@gmail.com>
2016-10-27 13:42:17 -0600

    Adds pull to refresh to the featured products page.

--- app/views/FeaturedProducts/index.js
+++ app/views/FeaturedProducts/index.js
@@ -2,30 +2,37 @@
 
 import React from 'react';
 import { connect } from 'react-redux';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, RefreshControl } from 'react-native';
 
+import { fetchFeed } from '../../actions';
 import Container from '../shared/Container';
 import Products from '../Products';
 
-class FeaturedProducts extends React.Component {
-  render() {
-    let { feed } = this.props;
+const FeaturedProducts = ({ feed, fetchFeed }) => (
+  <Container style={ styles.container }>
+    { feed.isFetching && feed.products.length === 0 ? (
+      <Text>Fetching Products</Text>
+    ) : (
+      <Products
+        products={ feed.products }
+        cardSize={ 'large' }
+        title={ 'FEATURED PRODUCTS' }
+        refreshControl={ <RefreshControl
+          refreshing={ feed.isFetching }
+          tintColor='#d4d9da'
+          onRefresh={ fetchFeed }
+        /> }
+      />
+    )}
+  </Container>
+);
 
-    return (
-      <Container style={ styles.container }>
-        { feed.isFetching ? (
-          <Text>Fetching Products</Text>
-        ) : (
-          <Products
-            products={ feed.products }
-            cardSize={ 'large' }
-            title={ 'FEATURED PRODUCTS' }
-          />
-        )}
-      </Container>
-    );
-  }
-}
+FeaturedProducts.propTypes = {
+  fetchFeed: React.PropTypes.func.isRequired,
+  feed: React.PropTypes.shape({
+    products: React.PropTypes.array.isRequired,
+  }).isRequired,
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -38,6 +45,8 @@ const mapStateToProps = state => ({
   feed: state.feed,
 });
 
-const mapActionsToProps = _ => ({});
+const mapActionsToProps = dispatch => ({
+  fetchFeed: () => dispatch(fetchFeed()),
+});
 
 export default connect(mapStateToProps, mapActionsToProps)(FeaturedProducts);
