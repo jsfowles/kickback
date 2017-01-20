7693a05999c9fd197593e9c61a1fe165164df657
jsfowles <js.fowles@gmail.com>
2017-01-20 14:27:54 -0700

    Changed appDelegate

--- ios/Kickback/AppDelegate.m
+++ ios/Kickback/AppDelegate.m
@@ -36,7 +36,7 @@
   // Riley @ Work:
   // jsCodeLocation = [NSURL URLWithString:@"http://192.168.1.236:8081/index.ios.bundle?platform=ios&dev=true"];
   // Jacob @ Work:
-   jsCodeLocation = [NSURL URLWithString:@"http://192.168.1.70:8081/index.ios.bundle?platform=ios&dev=true"];
+//   jsCodeLocation = [NSURL URLWithString:@"http://192.168.1.70:8081/index.ios.bundle?platform=ios&dev=true"];
 
   /**
    * OPTION 2
@@ -45,7 +45,7 @@
    * running the project on an actual device or running the project on the
    * simulator in the "Release" build configuration.
    */
-// jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+ jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"Kickback"
